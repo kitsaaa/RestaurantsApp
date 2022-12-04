08ee54bf4172eeba03b7bb7f88394721896d1cfe
sarvinraeisi <raeisisarvin@gmail.com>
2022-12-04 16:16:44 -0500

    signup front-end validation done

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AbstractType, Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
@@ -17,14 +17,38 @@ export class SignupComponent implements OnInit {
     this.signUpForm = this.formBuilder.group({
       firstName: ['', Validators.required],
       lastName: ['', Validators.required],
-      email: ['', Validators.required],
-      userName: ['', Validators.required],
-      password: ['', Validators.required]
-    })
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+      password: ['', Validators.compose([Validators.required, Validators.minLength(8)])],
+      ConfirmPassword: ['', Validators.required]
+    },
+    {
+      validators: this.MustMatch('password', 'ConfirmPassword')
+    }
+    )
   }
   hideShowPass(){
     this.isText = !this.isText;
     this.isText ? this.eyeIcon = "fa-eye" : this.eyeIcon = "fa-eye-slash";
     this.isText ? this.type = "text" : this.type = "password"
+  }
+
+  get f(){
+    return this.signUpForm.controls
+  }
+
+  MustMatch(password:any, ConfirmPassword:any){
+    return(fg:FormGroup)=>{
+      const passwordctrl = fg.controls[password];
+      const Confrimpasswordctrl = fg.controls[ConfirmPassword];
+      
+
+      if(passwordctrl.value != Confrimpasswordctrl.value){
+        Confrimpasswordctrl.setErrors({MustMatch: true});
+        console.log("inside if")
+      }
+      else{
+        Confrimpasswordctrl.setErrors(null);
+      }
+    }
   }
 }
