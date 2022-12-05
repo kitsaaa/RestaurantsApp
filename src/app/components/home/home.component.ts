1a8d328ef1ee03865010f363243e6a51ba17152e
Nikita <niggfg221@gmail.com>
2022-12-05 11:52:48 -0500

    added post rendering in home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IPost } from 'src/app/services/Post';
+import { PostsService } from 'src/app/services/posts.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  blogPosts: Array<IPost> | undefined;
+  querySub : any;
+  constructor(private data: PostsService,private route : ActivatedRoute) { }
+ 
+  getPage(num: number) {
+    this.querySub = this.data.getPosts(num).subscribe(data => {
+      if (data.length > 0) {
+        this.blogPosts = data;
+      }
+    });
   }
 
+  ngOnInit(): void {
+
+    this.querySub = this.route.queryParams.subscribe(params => {
+      this.getPage(+params['page'] || 1);
+      });
+  }
+
+  ngOnDestroy()
+  {
+    if(this.querySub) this.querySub.unsubscribe();
+  }
 }
