093b9871f55f8a824c10393650b7aa745dfe2df6
Maryam <71490610+mnajibi@users.noreply.github.com>
2023-01-27 19:25:39 +0000

    protected route added

--- src/app/components/navigate/navigate.component.ts
+++ src/app/components/navigate/navigate.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navigate',
@@ -7,7 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigateComponent implements OnInit {
   opened = false;
-  constructor() { }
+  constructor(private _router: Router) { }
+
+  addAdmin() {
+    this._router.navigate(['/dashboard/addadmin']);
+  }
+
+  addPost() {
+    this._router.navigate(['/dashboard/addpost']);
+  }
 
   ngOnInit(): void {
   }
